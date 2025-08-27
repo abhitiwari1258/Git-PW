9be7932aafb784c8862731521d51b188a6cabe6b
abhitiwari1258 <153221369+abhitiwari1258@users.noreply.github.com>
2025-08-27 14:09:44 +0530

    This is react App file

---
+++ App.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  const styles = {
+    main: {
+      padding: '20px',
+    },
+    title: {
+      color: '#5C6AC4'
+    },
+  };
+  
+  function ChatBot(){
+    return (
+       <>               
+        <input placeholder="Send a Messag To ChatBot" size="30" />
+        <button>Send</button>
+      </>
+      )
+  };
+  
+  function ChatMsg(props){
+    const Msg = props.message 
+    const sender = props.sender
+    return (
+      <div>
+        {Msg}
+        <img src="https://uploads.onecompiler.io/43t4rg6aq/43t4pprmy/user.png" width="50px" />
+      </div>
+      )
+  }
+
+  return (
+    <>
+      <ChatBot/>
+      <ChatMsg message="Hello ChatBot" sender="user" />
+      <ChatMsg message="Hello! How Can I Help U" sender="robot" />
+      <ChatMsg message="can u get me todays date" sender="user" />
+      <ChatMsg message="Today is August 8" sender="robot" />
+      <ChatMsg message="how about Flip a Coin" sender="user" />
+      <ChatMsg message="Sure! You got tails" sender="robot" />
+    </>
+  )
+}
+
+export default App
